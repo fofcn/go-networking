b500e3a91b7f89872561b636d63816dea1f50c0c
ford_ji <ford_ji@epam.com>
2024-03-27 13:14:06 +0800

    add unbuffered channel

---
+++ SQL_guild.md
@@ -0,0 +1,50 @@
+
+# Concepts
+- Database
+- Table
+- Record/Row
+- Column
+- SQL
+- Data Type
+- Index
+- Constraints
+- DDL
+- DML
+
+
+# SQL
+
+## INSERT
+
+## UPDATE
+
+## DELETE
+
+## SELECT
+
+### Basic
+
+##
+
+# Indexing
+
+# Optimization
+
+# Solve a problem
+1. [leetcode: Second Highest Salary](https://leetcode.com/problems/second-highest-salary/description/)
+
+
+# Remarks
+1. All major SQL databases have their own dialects(方言) （Science and Engineering ^_^）.
+![dialect and driver](./assets/dialect-and-driver-in-database.png)
+
+2. SQL standards
+![SQL Standards History](./assets/SQL-Standards.png)
+
+# References
+1. [SQL Standardization](https://en.wikipedia.org/wiki/SQL#Standardization)
+2. [MySQL Standards Compliance:8.0](https://dev.mysql.com/doc/refman/8.0/en/compatibility.html)
+3. [Difference between database drivers and database dialects](https://stackoverflow.com/questions/2085368/difference-between-database-drivers-and-database-dialects#:~:text=A%20database%20dialect%20is%20a,into%20vendor%20specific%20DDL%2C%20DML.)
+4. [The SQL Standard – ISO/IEC 9075:2023 (ANSI X3.135)](https://blog.ansi.org/sql-standard-iso-iec-9075-2023-ansi-x3-135/)
+5. [Database Indexing](https://en.wikipedia.org/wiki/Database_index)
+6. [DB Design](https://web.csulb.edu/colleges/coe/cecs/dbdesign/dbdesign.php?page=models.html)
--- learn/channel/channel_test.go
+++ learn/channel/channel_test.go
@@ -1,6 +1,7 @@
 package channel_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -29,19 +30,68 @@ import (
 //    接收方只有在有缓冲Channel为空时才会阻塞
 //    有缓冲通道的典型应用场景是生产者和消费者
 
+func TestUnbufferedChannel_ShouldPanic_whenWriteValueToAClosedChannel(t *testing.T) {
+
+	f := func() {
+		ch := make(chan int)
+		close(ch)
+
+		ch <- 1
+	}
+
+	assert.Panics(t, f, "should panic")
+}
+
+func TestUnbufferedChannel_ShouldSuccess_whenRecvValueAtAClosedChannel(t *testing.T) {
+	ch := make(chan int)
+	close(ch)
+	var val = <-ch
+	assert.Equal(t, 0, val)
+}
+
+func TestUnbufferedChannel_ShouldSuccess_whenRecvEmptyValueFromAClosedChannel(t *testing.T) {
+	ch := make(chan string)
+	close(ch)
+	var val = <-ch
+	assert.Equal(t, "", val)
+	val = <-ch
+	assert.Equal(t, "", val)
+}
+
+func TestUnbufferedChannel_ShouldReturnNil_whenRecvDataFromAClosedChannel(t *testing.T) {
+	var expectedStr *string = nil
+	ch := make(chan *string)
+	close(ch)
+	var val = <-ch
+	assert.Equal(t, expectedStr, val)
+	val = <-ch
+	assert.Equal(t, expectedStr, val)
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	// 将累加结果发送到channel
+	c <- sum
+}
+
 func TestUnbufferedChannel_ShouldRecvValues_WhenWriteValueToChannel(t *testing.T) {
 
-	// 创建channel
+	// 创建无缓冲channel
 	c := make(chan int)
 
 	// given
 	s := []int{1, 2, 3, 4, 5, 6}
 
 	// when
+	// 执行数组累加
 	go sum(s[:], c)
 	ret1 := <-c
 
 	// should
+	// 和应该是21
 	assert.Equal(t, 21, ret1)
 }
 
@@ -118,10 +168,66 @@ func TestBufferedChannel_ShouldBlock_WhenCapacityReached(t *testing.T) {
 	assert.True(t, is_block)
 }
 
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
+// 先定义一个worker函数
+// worker函数从无缓冲channel中接收
+// 可以接到到数据就执行后面的打印内容
+// 打印完成后退出
+func worker(id int, lock chan bool) {
+	var shouldRun = <-lock
+	if shouldRun {
+		fmt.Printf("time: %v Worker %d is working\n", time.Now(), id)
+		time.Sleep(time.Second)
+		fmt.Printf("time: %v Worker %d has finished\n", time.Now(), id)
 	}
-	c <- sum // send sum to c
+}
+
+func TestUnbufferedChannel_ShouldRunOneByOne_When(t *testing.T) {
+	lock := make(chan bool, 1)
+
+	// 启动5个goroutine等待释放接收
+	for i := 0; i < 5; i++ {
+		go worker(i, lock)
+	}
+
+	// 发送5个true到channel
+	for i := 0; i < 5; i++ {
+		lock <- true
+		time.Sleep(time.Second)
+	}
+
+	close(lock)
+
+	time.Sleep(10 * time.Second)
+}
+
+func future(id int, delay time.Duration, resChan chan int) {
+	time.Sleep(delay)
+	fmt.Printf("Hi, I have finished my task, my id is %d\n", id)
+	resChan <- id
+}
+
+func anyOf(futures ...<-chan int) <-chan int {
+	result := make(chan int)
+	for _, future := range futures {
+		go func(f <-chan int) {
+			result <- <-f
+		}(future)
+	}
+	return result
+}
+
+func TestAnyOf_ShouldSuccess(t *testing.T) {
+	// 创建无缓冲的 channel
+	resChan1 := make(chan int)
+	resChan2 := make(chan int)
+	resChan3 := make(chan int)
+
+	// 启动 goroutines
+	go future(1, 3*time.Second, resChan1)
+	go future(2, 2*time.Second, resChan2)
+	go future(3, 5*time.Second, resChan3)
+
+	result := anyOf(resChan1, resChan2, resChan3)
+
+	assert.Equal(t, 2, <-result)
 }
