d90705c1bf36fbf3736a1f623f2a7b8cada96fe6
ford_ji <ford_ji@epam.com>
2024-04-07 17:56:24 +0800

    optimize

--- network/connmanger.go
+++ network/connmanger.go
@@ -2,7 +2,6 @@ package network
 
 import (
 	"errors"
-	"go-networking/log"
 	"go-networking/network/codec"
 	"sync"
 	"sync/atomic"
@@ -50,9 +49,7 @@ type ConnManager struct {
 }
 
 // NewConnManager 创建并初始化一个新的ConnManager实例。
-// 返回值: 初始化后的ConnManager指针。
 func NewConnManager() *ConnManager {
-	atomic.StoreInt32(0)
 	cm := &ConnManager{
 		deviceConnMap: &sync.Map{},
 		timeout:       30 * time.Second,
@@ -60,21 +57,16 @@ func NewConnManager() *ConnManager {
 	}
 
 	cm.isStopped.Store(0)
-
-	cm.cleanupNoActiveConn()
+	go cm.cleanupNoActiveConn()
 	return cm
 }
 
 // Store 将连接存储到设备连接映射中。
-// id: 设备UID。
-// conn: 要存储的连接对象。
 func (cm *ConnManager) Store(id string, conn *Conn) {
 	cm.deviceConnMap.Store(id, newConnCtx(conn))
 }
 
 // StoreCKey 更新指定设备的CKey。
-// id: 设备UID。
-// cKey: 要更新的CKey。
 func (cm *ConnManager) StoreCKey(id string, cKey []byte) {
 	if value, ok := cm.deviceConnMap.Load(id); ok {
 		ctx := value.(*ConnCtx)
@@ -83,8 +75,6 @@ func (cm *ConnManager) StoreCKey(id string, cKey []byte) {
 }
 
 // StoreSKey 更新指定设备的SKey。
-// id: 设备UID。
-// sKey: 要更新的SKey。
 func (cm *ConnManager) StoreSKey(id string, sKey []byte) {
 	if value, ok := cm.deviceConnMap.Load(id); ok {
 		ctx := value.(*ConnCtx)
@@ -93,7 +83,6 @@ func (cm *ConnManager) StoreSKey(id string, sKey []byte) {
 }
 
 // Delete 从设备连接映射中删除指定的设备连接。
-// id: 设备UID。
 // 返回值: 被删除的ConnCtx实例，如果未找到则返回nil。
 func (cm *ConnManager) Delete(id string) *ConnCtx {
 	if value, ok := cm.deviceConnMap.Load(id); ok {
@@ -105,8 +94,6 @@ func (cm *ConnManager) Delete(id string) *ConnCtx {
 }
 
 // Load 根据设备UID加载对应的连接。
-// id: 设备UID。
-// 返回值: 对应的连接对象和一个布尔值，表示是否成功找到连接。
 func (cm *ConnManager) Load(id string) (*Conn, bool) {
 	if value, ok := cm.deviceConnMap.Load(id); ok {
 		ctx := value.(*ConnCtx)
@@ -117,8 +104,6 @@ func (cm *ConnManager) Load(id string) (*Conn, bool) {
 }
 
 // Ping 根据设备UID标记该设备连接为活跃。
-// id: 设备UID。
-// 返回值: 错误对象，如果设备未找到则返回错误。
 func (cm *ConnManager) Ping(id string, ts int64) error {
 	if time.Now().Unix()-ts > int64(cm.timeout/time.Second) {
 		return codec.Invalid_Ping_Frame
@@ -137,31 +122,26 @@ func (cm *ConnManager) Ping(id string, ts int64) error {
 // Stop 停止ConnManager的定时清理任务。
 func (cm *ConnManager) Stop() {
 	cm.isStopped.Store(1)
-	defer cm.timer.Stop()
+	cm.timer.Stop()
 }
 
 // cleanupNoActiveConn 启动一个goroutine定期清理长时间未活跃的连接。
 func (cm *ConnManager) cleanupNoActiveConn() {
-	go cm.doCleanupNoActiveConn()
-}
-
-// doCleanupNoActiveConn 实际执行清理无活跃连接的任务。
-func (cm *ConnManager) doCleanupNoActiveConn() {
-	println("enter cleanup timer")
 	for {
-		<-cm.timer.C
-		if cm.isStopped.Load() == 0 {
-			log.Info("cleanup timer has triggered")
-			cm.deviceConnMap.Range(func(k, v interface{}) bool {
-				now := time.Now().Unix()
-				if connctx, ok := v.(*ConnCtx); ok {
-					if now-connctx.LastPingTime > int64(cm.timeout/time.Second) {
-						cm.deviceConnMap.Delete(k)
+		select {
+		case <-cm.timer.C:
+			if cm.isStopped.Load() == 0 {
+				cm.deviceConnMap.Range(func(k, v interface{}) bool {
+					now := time.Now().Unix()
+					if connctx, ok := v.(*ConnCtx); ok {
+						if now-connctx.LastPingTime > int64(cm.timeout/time.Second) {
+							cm.deviceConnMap.Delete(k)
+						}
 					}
-				}
 
-				return true
-			})
+					return true
+				})
+			}
 		}
 	}
 }
